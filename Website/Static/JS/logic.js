29556ecaabb22be748ae4535fa14d083f5e87bb5
MLutfor <142198802+MLutfor@users.noreply.github.com>
2023-12-09 13:38:20 -0500

    volume chart

--- Website/Static/JS/logic.js
+++ Website/Static/JS/logic.js
@@ -1,4 +1,3 @@
-
 const jsonFileURL = "http://127.0.0.1:5000/csv_to_json";
 
 Tickers= []
@@ -6,116 +5,68 @@ Tickers= []
 d3.json(jsonFileURL).then(function(data) {
     // The ‘data’ variable now contains the parsed JSON data
 
-
     data.forEach(function(data){
         data.Close = parseFloat(data.Close)
         data.Open = parseFloat(data.Open)
         data.High = parseFloat(data.High)
         data.Low = parseFloat(data.Low)
         data.Volume = parseInt(data.Volume, 10)
-    
+    });
 
-    })
+    console.log(data);
 
+    candlystick("COST", data);
 
- console.log(data);
-
-    candlystick("COST",data)
- 
-
-  
-  
-
-
-
-
-
-
-
-
-      // Function to handle button clicks on the graph
-      document.querySelectorAll('.time-button').forEach(button => {
+    // Function to handle button clicks on the graph
+    document.querySelectorAll('.time-button').forEach(button => {
         button.addEventListener('click', function() {
-          this.classList.toggle('active'); // Toggle 'active' class
+            this.classList.toggle('active'); // Toggle 'active' class
         });
-      });
-  
-      // Function to update the graph data based on the selected time period
-      function updateGraph(timePeriod) {
+    });
+
+    // Function to update the graph data based on the selected time period
+    function updateGraph(timePeriod) {
         // Add your logic here to update the graph data
         // This function will be triggered when a button on the graph is clicked
         console.log('Updating graph for:', timePeriod);
-      }
+    }
 
-      function handleButtonClick(arg){
+    function handleButtonClick(arg) {
 
-      }
-
-
-////////////////////////////
-
-
+    }
 
     // Example: Display data in the console
-for (i=0; i < data.length; i++){
-
-Tickers.push(data[i]);
-
-}
-
-//////////////////////
-
-
+    for (i = 0; i < data.length; i++) {
+        Tickers.push(data[i]);
+    }
 
     data.forEach(function(item) {
         // console.log(item);
     });
     // Example: Render data on the webpage (modify as needed)
-    
-    
-    
-    
-    
     renderData(data);
 }).catch(function(error) {
     // Handle errors, if any
     console.error("Error loading JSON file:", error);
 });
 
+function candlystick(selectedvalue, data) {
+    let filter_data2 = data.filter((data) => data.Ticker == selectedvalue);
 
+    let Tickers_dates = [];
+    let Ticker_High = [];
+    let Ticker_Low = [];
+    let Ticker_Open = [];
+    let Ticker_Close = [];
+    let Ticker_Volume = [];
 
-
-// function Ticker_Selection(TCKR){
-
-//     if (data.ticker == TCKR){
-//         return data
-//     }
-
-
-// }
-
-
-
-function candlystick(selectedvalue,data){
-    let filter_data2=data.filter((data) => data.Ticker==selectedvalue)
-
-    Tickers_dates = [];
-    Ticker_High= [];
-    Ticker_Low= [];
-    Ticker_Open = [];
-    Ticker_Close= [];
-
-    
-    for (let i =0; i < filter_data2.length; i++){
-
+    for (let i = 0; i < filter_data2.length; i++) {
         Tickers_dates.push(filter_data2[i].Date);
         Ticker_Close.push(filter_data2[i].Close);
         Ticker_Open.push(filter_data2[i].Open);
         Ticker_High.push(filter_data2[i].High);
         Ticker_Low.push(filter_data2[i].Low);
-
-        // console.log(AAL[i].dates)
-
+        Ticker_Volume.push(filter_data2[i].Volume);
     }
 
     var trace = {
@@ -124,51 +75,66 @@ function candlystick(selectedvalue,data){
         high: Ticker_High,
         low: Ticker_Low,
         close: Ticker_Close,
-      
-        // cutomise colors
-        increasing: {line: {color: 'black'}},
-        decreasing: {line: {color: 'red'}},
-      
+        increasing: { line: { color: 'black' } },
+        decreasing: { line: { color: 'red' } },
         type: 'candlestick',
         xaxis: 'x',
-        yaxis: 'y'
-      };
-      
-      var data = [trace];
-      
-      var layout = {
+        yaxis: 'y',
+        name: 'Candlestick'
+    };
+
+    // Candlestick chart
+    var dataCandlestick = [trace];
+
+    var layout = {
         dragmode: 'zoom',
-        showlegend: false,
+        showlegend: true,
+        legend: {
+            x: 0,
+            y: 1,
+            orientation: 'h'
+        },
         xaxis: {
-          rangeslider: {
-               visible: false
+            rangeslider: {
+                visible: false
             }
-          },
-          title: {
-              text: `Candlestick Chart for ${selectedvalue}` // Title based on selected ticker
-          }
-      };
+        },
+        title: {
+            text: `Candlestick Chart for ${selectedvalue}`
+        }
+    };
 
+    // Volume chart
+    var traceVolume = {
+        x: Tickers_dates,
+        y: Ticker_Volume,
+        type: 'bar',
+        yaxis: 'y2',
+        name: 'Volume'
+    };
 
-      Plotly.newPlot('Historic_12_Month_Run', data, layout);
+    var dataVolume = [traceVolume];
 
+    var layoutVolume = {
+        yaxis2: {
+            title: 'Volume',
+            overlaying: 'y',
+            side: 'right'
+        }
+    };
+
+    Plotly.newPlot('Historic_12_Month_Run', dataCandlestick, layout);
+    Plotly.newPlot('volumeChart', dataVolume, layoutVolume);
 }
 
-
 // Calling existing "optionChanged" function from HTML class to pass selected value through all my 3 functions (bubble,bar and dinfo functions)
-function optionChanged(selectedvalue){
+function optionChanged(selectedvalue) {
     console.log(selectedvalue)
-  
+
     d3.json(jsonFileURL).then((data) => {
-  
-  // Passing user selected value from the drop down and the data through each function to populate the graphs dynamically 
-   candlystick(selectedvalue, data)
-   
-  });
-  
-  
-  
-  
-  
-     
-    }
+
+        // Passing user selected value from the drop down and the data through each function to populate the graphs dynamically 
+        candlystick(selectedvalue, data)
+
+    });
+}
