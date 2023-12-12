95d8d4b4478fa4fc5b1cbee3f8b62bacb70c0ed3
MLutfor <142198802+MLutfor@users.noreply.github.com>
2023-12-11 22:26:35 -0500

    current ticker

--- Website/Static/JS/logic.js
+++ Website/Static/JS/logic.js
@@ -2,7 +2,7 @@
 const jsonFileURL = "http://127.0.0.1:5000/csv_to_json";
 const json_news = "http://127.0.0.1:5000/Ticker_News";
 
-// Tickers= []
+let currentTicker='AAL'
 // Use d3.json() to load the JSON file
 d3.json(jsonFileURL).then(function(data) {
     // The ‘data’ variable now contains the parsed JSON data
