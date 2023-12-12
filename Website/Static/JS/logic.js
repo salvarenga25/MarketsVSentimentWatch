8f4119dde80f25e2354e66bcdcaf520f9b6caaf2
MLutfor <142198802+MLutfor@users.noreply.github.com>
2023-12-11 22:50:28 -0500

    variable

--- Website/Static/JS/logic.js
+++ Website/Static/JS/logic.js
@@ -2,10 +2,12 @@
 const jsonFileURL = "http://127.0.0.1:5000/csv_to_json";
 const json_news = "http://127.0.0.1:5000/Ticker_News";
 
+//global variable
 let currentTicker='AAL'
 // Use d3.json() to load the JSON file
 d3.json(jsonFileURL).then(function(data) {
     // The ‘data’ variable now contains the parsed JSON data
+
 
 
     data.forEach(function(data){
